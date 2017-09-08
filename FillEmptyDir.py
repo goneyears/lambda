99f7f7283bb2cc039a535d1a650e9ee5d4707dbd
GoneYears <goneyears@qq.com>
2017-09-08 15:13:03 +0800

    Add all code files. Modify FillEmptyDir function

--- FillEmptyDir.py
+++ FillEmptyDir.py
@@ -10,22 +10,22 @@ def fillEmptyDir(curdir):
     dirfiles=os.listdir(curdir)
     #print(len(dirfiles))
     if not dirfiles:
-        print('add git.empty file: '+curdir+'\git.empty')
+        print('Create git.empty file: '+curdir+'\git.empty')
         AddGitIgnoreFile(curdir)
         return
     else:
         if(os.path.exists(os.path.join(curdir, 'git.empty')) and len(dirfiles)>1 ):
             os.remove(os.path.join(curdir, 'git.empty'))
-            print('remove git.empty file:'+curdir+'\git.empty')
-    list = os.listdir(curdir)  
-    for i in range(0, len(list)):
-        subdir = os.path.join(curdir, list[i])
+            print('REMOVE git.empty file:'+curdir+'\git.empty')
+
+    for i in range(0, len(dirfiles)):
+        subdir = os.path.join(curdir, dirfiles[i])
         if os.path.isdir(subdir):
             fillEmptyDir(subdir)
     return
 
 
 fillEmptyDir(os.getcwd())
-input("\nPress the enter key to exit.")
+input("\nPress the ENTER key to exit.")
 
 
