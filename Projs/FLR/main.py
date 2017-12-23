819ca0ae960a39ea089428f6035bf2b297245063
GoneYears <goneyears@qq.com>
2017-12-23 23:11:31 +0800

    control in window test complete

--- Projs/FLR/main.py
+++ Projs/FLR/main.py
@@ -9,7 +9,7 @@ from matrix import *
 
 ui = maindesign.Ui_MainWindow()
 brun = 0
-
+lbls = []
 def read_edit():
     return str(ui.textEdit.toPlainText())
 
@@ -29,9 +29,11 @@ def backspace_edit():
     ui.textEdit.setFocus()
 
 sta = 'up'
-xpos = 60
-ypos = 640
-
+xpos = 0
+ypos = 0
+xgap = 0
+ygap =0
+planegeo = QtCore.QRect(0,0,0,0)
 def leftrotatemap(curstate):
     switcher = {
         'up': leftstate,
@@ -90,37 +92,50 @@ def upstate():
 
 def upmove():
     global xpos,ypos
-    ypos = ypos - 155
+    ypos = ypos - ygap
+
     mx.up()
 
 
 def downmove():
     global xpos,ypos
-    ypos = ypos + 155
+    ypos = ypos + ygap
     mx.down()
 
 def leftmove():
     global xpos,ypos
-    xpos = xpos - 150
+    xpos = xpos - xgap
     mx.left()
 
 def rightmove():
     global xpos,ypos
-    xpos = xpos + 150
+    xpos = xpos + xgap
     mx.right()
 
 def leftturn():
+
+    ui.aeroplane_label.hide()
     leftrotatemap(sta)()
-    time.sleep(0.3)
+    ui.aeroplane_label.show()
+    updategeo()
 
 def rightturn():
     rightrotatemap(sta)()
-    time.sleep(0.3)
+    updategeo()
 
 def forward():
-    global xpos, ypos
+    global xpos,ypos
+    global planegeo
     movemap(sta)()
-    ui.aeroplane_label.setGeometry(QtCore.QRect(xpos, ypos, 181, 181))
+    updategeo()
+
+def updategeo():
+    global xpos,ypos
+    global planegeo
+    time.sleep(0.0001)
+    planegeo.moveTopLeft(QtCore.QPoint(xpos,ypos))
+    print('planegeo movetopleft:'+str(planegeo))
+    ui.aeroplane_label.setGeometry(planegeo)
     time.sleep(0.3)
 
 def see(dir):
@@ -247,8 +262,15 @@ def Newline_button_click():
 def Backspace_button_click():
     backspace_edit()
 
-def Clear_button_click():
-    clear_edit()
+t = QtCore.QTimer()
+t.timeout.connect(clear_edit)
+def Clear_button_press():
+    print('clear press')
+    t.start(2000)
+
+def Clear_button_release():
+    print('clear release')
+    t.stop()
 
 def Run_button_click():
     global brun
@@ -277,156 +299,67 @@ def thread1():
             print('code syntax error')
         time.sleep(0.1)
 
-class Rect:
-    def __init__(self, posx, posy, width, height):
-        self.x = posx
-        self.y = posy
-        self.w = width
-        self.h = height
-        self.rect = QtCore.QRect(self.x, self.y, self.w, self.h)
+bresize = 0
+def threadresize():
+    global bresize
+    while(True):
+        # try:
+        if bresize == 1:
+            bresize = 0
+            time.sleep(0.5)
+            if bresize == 0:
+                uiinitial()
 
-def compoinit():
+        time.sleep(0.5)
+def uiinitial():
+    global planegeo
+    global xgap, ygap
+    global xpos, ypos
+    global lbls
+    planegeo = ui.label15.geometry()
+    xpos = planegeo.left()
+    ypos = planegeo.top()
+    xgap = planegeo.width()
+    ygap = planegeo.height()
+    ui.aeroplane_label.setPixmap(QtGui.QPixmap("res/up.png"))
+    time.sleep(0.6)
+    ui.aeroplane_label.setPixmap(QtGui.QPixmap("res/left.png"))
+    time.sleep(0.3)
+    ui.aeroplane_label.setPixmap(QtGui.QPixmap("res/down.png"))
+    time.sleep(0.3)
+    ui.aeroplane_label.setPixmap(QtGui.QPixmap("res/right.png"))
+    time.sleep(0.3)
+    ui.aeroplane_label.setPixmap(QtGui.QPixmap("res/up.png"))
+    time.sleep(0.3)
+    ui.aeroplane_label.setGeometry(planegeo)
 
-    aeroplane = Rect(60, 640, 100, 100)
+    lbls[mx.curpos[0]][mx.curpos[1]].setPixmap(QtGui.QPixmap("res/up.png"))
+    print('planegeo initial:'+str(planegeo))
 
-    label11 = Rect(50, 30, 150, 150)
-    label12 = Rect(50, 180, 150, 150)
-    label13 = Rect(50, 330, 150, 150)
-    label14 = Rect(50, 480, 150, 150)
-    label15 = Rect(50, 630, 150, 150)
+class nQMainWindow(QMainWindow):
 
-    label21 = Rect(200, 30, 150, 150)
-    label22 = Rect(200, 180, 150, 150)
-    label23 = Rect(200, 330, 150, 150)
-    label24 = Rect(200, 480, 150, 150)
-    label25 = Rect(200, 630, 150, 150)
+    def resizeEvent(self, a0: QtGui.QResizeEvent):
+        global bresize
+        bresize = 1
+        print('resize')
 
-    label31 = Rect(350, 30, 150, 150)
-    label32 = Rect(350, 180, 150, 150)
-    label33 = Rect(350, 330, 150, 150)
-    label34 = Rect(350, 480, 150, 150)
-    label35 = Rect(350, 630, 150, 150)
-
-    label41 = Rect(500, 30, 150, 150)
-    label42 = Rect(500, 180, 150, 150)
-    label43 = Rect(500, 330, 150, 150)
-    label44 = Rect(500, 480, 150, 150)
-    label45 = Rect(500, 630, 150, 150)
-
-    label51 = Rect(650, 30, 150, 150)
-    label52 = Rect(650, 180, 150, 150)
-    label53 = Rect(650, 330, 150, 150)
-    label54 = Rect(650, 480, 150, 150)
-    label55 = Rect(650, 630, 150, 150)
-
-    ClearButton = Rect(60, 880, 131, 111)
-
-    textEdit = Rect(910, 30, 1121, 821)
-
-    N1Button = Rect(910, 890, 131, 111)
-    N2Button = Rect(1050, 890, 131, 111)
-    N3Button = Rect(1190, 890, 131, 111)
-    N5Button = Rect(1470, 890, 131, 111)
-    N4Button = Rect(1330, 890, 131, 111)
-    BackspaceButton = Rect(1610, 890, 131, 111)
-    NewlineButton = Rect(1850, 890, 131, 111)
-
-    FButton = Rect(910, 1030, 131, 111)
-
-    LButton = Rect(1050, 1030, 131, 111)
-    RButton = Rect(1190, 1030, 131, 111)
-    BButton = Rect(1330, 1030, 131, 111)
-    LBracketButton = Rect(1470, 1030, 131, 111)
-    RBracketButton = Rect(1610, 1030, 131, 111)
-    RunButton = Rect(1850, 1030, 131, 111)
-
-    NButton = Rect(910, 1160, 131, 111)
-    SButton = Rect(1050, 1160, 131, 111)
-    UButton = Rect(1190, 1160, 131, 111)
-    EqualButton = Rect(1330, 1160, 131, 111)
-    RMBracketButton = Rect(1610, 1160, 131, 111)
-    LMBracketButton = Rect(1470, 1160, 131, 111)
-    StopButton = Rect(1850, 1160, 131, 111)
-
-    IfButton = Rect(910, 1280, 131, 111)
-    WhileButton = Rect(1260, 1280, 201, 111)
-    ElseButton = Rect(1050, 1280, 201, 111)
-
-    menubar = Rect(0, 0, 2053, 38)
-
-    ui.aeroplane_label.setGeometry(aeroplane.rect)
-
-    ui.label11.setGeometry(label11.rect)
-    ui.label12.setGeometry(label12.rect)
-    ui.label13.setGeometry(label13.rect)
-    ui.label14.setGeometry(label14.rect)
-    ui.label15.setGeometry(label15.rect)
-
-    ui.label21.setGeometry(label21.rect)
-    ui.label22.setGeometry(label22.rect)
-    ui.label23.setGeometry(label23.rect)
-    ui.label24.setGeometry(label24.rect)
-    ui.label25.setGeometry(label25.rect)
-
-    ui.label31.setGeometry(label31.rect)
-    ui.label32.setGeometry(label32.rect)
-    ui.label33.setGeometry(label33.rect)
-    ui.label34.setGeometry(label34.rect)
-    ui.label35.setGeometry(label35.rect)
-
-    ui.label41.setGeometry(label41.rect)
-    ui.label42.setGeometry(label42.rect)
-    ui.label43.setGeometry(label43.rect)
-    ui.label44.setGeometry(label44.rect)
-    ui.label45.setGeometry(label45.rect)
-
-    ui.label51.setGeometry(label51.rect)
-    ui.label52.setGeometry(label52.rect)
-    ui.label53.setGeometry(label53.rect)
-    ui.label54.setGeometry(label54.rect)
-    ui.label55.setGeometry(label55.rect)
-
-    ui.ClearButton.setGeometry(ClearButton.rect)
-
-    ui.textEdit.setGeometry(textEdit.rect)
-
-    ui.N1Button.setGeometry(N1Button.rect)
-    ui.N2Button.setGeometry(N2Button.rect)
-    ui.N3Button.setGeometry(N3Button.rect)
-    ui.N5Button.setGeometry(N5Button.rect)
-    ui.N4Button.setGeometry(N4Button.rect)
-    ui.BackspaceButton.setGeometry(BackspaceButton.rect)
-    ui.NewlineButton.setGeometry(NewlineButton.rect)
-
-    ui.FButton.setGeometry(FButton.rect)
-
-    ui.LButton.setGeometry(LButton.rect)
-    ui.RButton.setGeometry(RButton.rect)
-    ui.BButton.setGeometry(BButton.rect)
-    ui.LBracketButton.setGeometry(LBracketButton.rect)
-    ui.RBracketButton.setGeometry(RBracketButton.rect)
-    ui.RunButton.setGeometry(RunButton.rect)
-
-    ui.NButton.setGeometry(NButton.rect)
-    ui.SButton.setGeometry(SButton.rect)
-    ui.UButton.setGeometry(UButton.rect)
-    ui.EqualButton.setGeometry(EqualButton.rect)
-    ui.RMBracketButton.setGeometry(RMBracketButton.rect)
-    ui.LMBracketButton.setGeometry(LMBracketButton.rect)
-    ui.StopButton.setGeometry(StopButton.rect)
-
-    ui.IfButton.setGeometry(IfButton.rect)
-    ui.WhileButton.setGeometry(WhileButton.rect)
-    ui.ElseButton.setGeometry(ElseButton.rect)
-
-    ui.menubar.setGeometry(menubar.rect)
 
 if __name__ =='__main__':
     app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
+    MainWindow = nQMainWindow()
     #ui = maindesign.Ui_MainWindow()
     ui.setupUi(MainWindow)
 
+    lbls = [
+
+        [None, None, None, None, None, None, None],
+        [None, ui.label11, ui.label21, ui.label31, ui.label41, ui.label51, None],
+        [None, ui.label12, ui.label22, ui.label32, ui.label42, ui.label52, None],
+        [None, ui.label13, ui.label23, ui.label33, ui.label43, ui.label53, None],
+        [None, ui.label14, ui.label24, ui.label34, ui.label44, ui.label54, None],
+        [None, ui.label15, ui.label25, ui.label35, ui.label45, ui.label55, None],
+        [None, None, None, None, None, None, None],
+    ]
 
     ui.FButton.clicked.connect(F_button_click)
     ui.LButton.clicked.connect(L_button_click)
@@ -444,11 +377,12 @@ if __name__ =='__main__':
     ui.ElseButton.clicked.connect(Else_button_click)
     ui.WhileButton.clicked.connect(While_button_click)
 
-
-    ui.BackspaceButton.clicked.connect(Backspace_button_click)
     ui.NewlineButton.clicked.connect(Newline_button_click)
 
-    ui.ClearButton.clicked.connect(Clear_button_click)
+    ui.BackspaceButton.clicked.connect(Backspace_button_click)
+    ui.BackspaceButton.pressed.connect(Clear_button_press)
+    ui.BackspaceButton.released.connect(Clear_button_release)
+
     ui.RunButton.clicked.connect(Run_button_click)
     ui.StopButton.clicked.connect(Stop_button_click)
     ui.N1Button.clicked.connect(N1_button_click)
@@ -467,12 +401,15 @@ if __name__ =='__main__':
     exes.Sevent.append(see)
     exes.Runevent.append(isrun)
 
-    MainWindow.show()
 
+    MainWindow.show()
     t1 = threading.Thread(target=thread1)
     t1.setDaemon(True)
     t1.start()
 
-
+    #this thread used to check window resizing
+    t2 = threading.Thread(target=threadresize)
+    t2.setDaemon(True)
+    t2.start()
     sys.exit(app.exec_())
 
